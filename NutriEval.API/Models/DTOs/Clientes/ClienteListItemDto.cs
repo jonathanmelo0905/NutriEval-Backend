14932be25ed745d1864a098bf41d0cbec77fe771
jonathanmelo0905 <jonathanmelo0905@gmail.com>
2026-05-22 00:40:56 -0500

    estatura

--- NutriEval.API/Models/DTOs/Clientes/ClienteListItemDto.cs
+++ NutriEval.API/Models/DTOs/Clientes/ClienteListItemDto.cs
@@ -9,5 +9,6 @@ public class ClienteListItemDto
     public string? Nivel { get; set; }
     public decimal? PesoInicial { get; set; }
     public bool Activo { get; set; }
+    public decimal? estatura { get; set; }
     public DateTimeOffset CreatedAt { get; set; }
 }
